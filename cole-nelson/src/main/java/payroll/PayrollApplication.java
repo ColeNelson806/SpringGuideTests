164c845a502e5de8bca71ff0d4d0fb40edea18a4
ColeNelson806 <colenelson806@outlook.com>
2023-09-11 14:35:23 -0500

    added hateoas dependancy

--- cole-nelson/src/main/java/payroll/PayrollApplication.java
+++ cole-nelson/src/main/java/payroll/PayrollApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PayrollApplication {
 
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		SpringApplication.run(PayrollApplication.class, args);
 	}
-
 }
